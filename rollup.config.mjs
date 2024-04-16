e86988cd968f7c32491f14bc51fb15ffb34fc4f5
taeo <ohtk1119@gmail.com>
2024-04-16 16:12:57 +0900

    feat 로컬에서 테스트할 example project 설정

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,16 +5,6 @@
  * @since 2022.06.06 Mon 17:44:31
  */
 
-// const pkg = require("./package.json");
-// const typescript = require("rollup-plugin-typescript2");
-// const peerDepsExternal = require("rollup-plugin-peer-deps-external");
-// const postcss = require("rollup-plugin-postcss");
-// const postcssPrefixer = require("postcss-prefixer");
-// const resolve = require("@rollup/plugin-node-resolve");
-// const commonjs = require("@rollup/plugin-commonjs");
-// const json = require("@rollup/plugin-json");
-// const babel = require("@rollup/plugin-babel");
-
 import pkg from "./package.json" assert { type: "json" };
 import typescript from "rollup-plugin-typescript2";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
@@ -83,60 +73,5 @@ export default {
       ],
     }),
   ],
-  external: ["react", "react/jsx-runtime"],
+  external: [...Object.keys(pkg.peerDependencies || {})],
 };
-
-// function setUpRollup({ input, output }) {
-//   return {
-//     input,
-//     exports: "named",
-//     output,
-//     watch: {
-//       include: "*",
-//       exclude: "node_modules/**",
-//     },
-//     plugins: [
-//       babel({
-//         babelHelpers: "bundled",
-//         presets: ["@babel/preset-env", "@babel/preset-react"],
-//       }),
-//       peerDepsExternal(),
-//       json(),
-//       resolve({ extensions }),
-//       commonjs(), // commonjs 플러그인을 설정하여 CommonJS 모듈을 올바르게 변환
-//       typescript({ useTsconfigDeclarationDir: true }),
-//       postcss({
-//         extract: true,
-//         modules: true,
-//         sourceMap: true,
-//         use: ["sass"],
-//         plugins: [
-//           postcssPrefixer({
-//             prefix: `${pkg.name}__`,
-//           }),
-//         ],
-//       }),
-//     ],
-//     external: ["react", "react-dom"],
-//   };
-// }
-
-// export default [
-//   setUpRollup({
-//     input: "index.ts",
-//     output: {
-//       file: "./dist/cjs.js",
-//       format: "cjs",
-//       sourcemap: true,
-//       preserveModulesRoot: "src",
-//     },
-//   }),
-//   setUpRollup({
-//     input: "index.ts",
-//     output: {
-//       file: "./dist/esm.js",
-//       format: "es",
-//       sourcemap: true,
-//     },
-//   }),
-// ];
