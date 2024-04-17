d18b1b7675c84b0cea29e7bc732ce81be39fc575
taeo <ohtk1119@gmail.com>
2024-04-17 18:42:36 +0900

    feat Add global type about typography el

--- src/global/index.d.ts
+++ src/global/index.d.ts
@@ -1,6 +1,9 @@
 import { MouseEvent } from "react";
+import { colors } from "../..";
 
 declare global {
   export type ButtonEvent = MouseEvent<HTMLButtonElement>;
   export type ClickEvent = (event: MouseEvent<HTMLButtonElement>) => void;
+  export type Colors = { [key in string]: T };
+  export type TypographyElement = HTMLSpanElement | HTMLHeadingElement;
 }
