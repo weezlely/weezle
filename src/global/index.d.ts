9fa01a540c45a4eaccd6b3c57cd0c4c68db77278
taeo <ohtk1119@gmail.com>
2024-04-18 21:21:07 +0900

    chore Update size, color type

--- src/global/index.d.ts
+++ src/global/index.d.ts
@@ -2,6 +2,8 @@ import { MouseEvent } from "react";
 import { colors } from "../..";
 
 declare global {
+  export type Variant = "primary" | "secondary" | "default";
+  export type Size = "xs" | "sm" | "md" | "lg" | "xl" | "xxl" | "xxxl";
   export type ButtonEvent = MouseEvent<HTMLButtonElement>;
   export type ClickEvent = (event: MouseEvent<HTMLButtonElement>) => void;
   export type Colors = { [key in string]: T };
