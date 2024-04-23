824eba1759deed99ad8778bd6803b0468c5462c2
taeo <ohtk1119@gmail.com>
2024-04-23 11:03:12 +0900

    chore: Update Click Event

--- src/global/index.d.ts
+++ src/global/index.d.ts
@@ -4,7 +4,7 @@ import { fontWeights } from "styles";
 declare global {
   export type Variant = "primary" | "secondary" | "default";
   export type Size = "xs" | "sm" | "md" | "lg" | "xl" | "xxl" | "xxxl";
-  export type ButtonEvent = MouseEvent<HTMLButtonElement>;
+  export type ButtonEvent = MouseEvent<HTMLButtonElement> | MouseEvent<HTMLInputElement>;
   export type ClickEvent = (event: MouseEvent<HTMLButtonElement>) => void;
   export type Color = string;
   export type Colors = { [key in string]: string };
