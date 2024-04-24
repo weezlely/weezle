5c236e56c535cbc6e69df58f32e5c9170f1d5042
taeo <ohtk1119@gmail.com>
2024-04-25 08:49:11 +0900

    test: Tset flyout

--- example/src/components/test-flyout.tsx
+++ example/src/components/test-flyout.tsx
@@ -1,23 +1,24 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { Flyout } from "taeo-ui";
 
 const TestFlyout = () => {
-  const [isOn, toggle] = React.useState<boolean>(false);
-
-  console.log(isOn);
   return (
-    <Flyout isOpen={isOn} toggle={toggle} style={{ width: "200px" }}>
-      <Flyout.Toggle>클릭</Flyout.Toggle>
+    <Flyout style={{ width: "200px" }}>
+      <Flyout.Toggle>토글</Flyout.Toggle>
       <Flyout.Overlay />
-      <Flyout.List style={{ width: "200px" }}>
-        <Flyout.Header>헤더</Flyout.Header>
-        <Flyout.Item id={1}>아이템1</Flyout.Item>
-        <Flyout.Item id={2}>아이템2</Flyout.Item>
-        <Flyout.Item id={3}>아이템3</Flyout.Item>
-        <Flyout.Item id={4}>아이템4</Flyout.Item>
+      <Flyout.List style={{ width: "200px", ...a }}>
+        <Flyout.Header>Flyout's Header</Flyout.Header>
+        <Flyout.Item id={1}>Flyout's Item-1</Flyout.Item>
+        <Flyout.Item id={2}>Flyout's Item-2</Flyout.Item>
+        <Flyout.Item id={3}>Flyout's Item-3</Flyout.Item>
+        <Flyout.Item id={4}>Flyout's Item-4</Flyout.Item>
+        <Flyout.Item id={5}>Flyout's Item-5</Flyout.Item>
+        <Flyout.Item id={6}>Flyout's Item-6</Flyout.Item>
       </Flyout.List>
     </Flyout>
   );
 };
 
 export default TestFlyout;
+
+const a: CSSProperties = {};
