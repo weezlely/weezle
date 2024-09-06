b76a157afa884f144f8e346993c45ed0ebcfa2bb
taeo <ohtk1119@gmail.com>
2024-09-06 13:54:24 +0900

    [ADD] UI에 필요한 커스텀 훅

---
+++ package/src/hooks/useToggle.ts
@@ -0,0 +1,24 @@
+import { useCallback, useState } from "react";
+
+export default function useToggle() {
+  const [isOpen, toggle] = useState<boolean>(false);
+
+  const onOpen = useCallback(() => {
+    toggle(true);
+  }, []);
+
+  const onClose = useCallback(() => {
+    toggle(false);
+  }, []);
+
+  const onToggle = useCallback(() => {
+    toggle(!isOpen);
+  }, []);
+
+  return {
+    isOpen,
+    onOpen,
+    onClose,
+    onToggle,
+  };
+}
